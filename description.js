20de6e4cafabbf646770dca56970c11caece7a69
Sergey <sinzem@gmail.com>
2024-08-20 21:53:42 +0300

    started_form

---
+++ description.js
@@ -0,0 +1,5 @@
+// Урок по созданию форм с помощью Formik и Yup(отдельный модуль для валидации)
+
+// Документация:
+// https://formik.org/docs/overview  --  npm install formik --save
+// https://github.com/jquense/yup
